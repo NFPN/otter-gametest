d615311797b8cb1a76b8aa54652d700bdd68a8cb
Natanael Nunes <natanaelfpnunes@gmail.com>
2019-11-09 11:27:48 -0300

    Add: otter base

---
+++ GameSetup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
